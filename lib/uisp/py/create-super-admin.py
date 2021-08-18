aa4b48d530f27aa65d56aff53fa7e9b80726b1fb
Reilly Chase <ReillyChase@MacBook-Pro.local>
2021-08-18 15:17:19 -0400

    Decode hash for string concat

--- lib/uisp/py/create-super-admin.py
+++ lib/uisp/py/create-super-admin.py
@@ -13,7 +13,7 @@ password = ''.join(random.choice(letters) for i in range(8))
 random_uuid = str(uuid.uuid4())
 bcrypt_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 site_group_id= os.popen('docker exec -it unms-postgres psql -U unms -d unms -t -c "SELECT group_id FROM access_group_site LIMIT 1;"').read()
-astr = "docker exec -it unms-postgres psql -U unms -c \"INSERT INTO unms.user (id,username,email,password,role,site_group_id) VALUES ('" + random_uuid + "','" + args.username + "','" + args.username + "@hostifi.com','" + bcrypt_hash + "','superadmin', '" + str(site_group_id).strip() + "');\""
+astr = "docker exec -it unms-postgres psql -U unms -c \"INSERT INTO unms.user (id,username,email,password,role,site_group_id) VALUES ('" + random_uuid + "','" + args.username + "','" + args.username + "@hostifi.com','" + bcrypt_hash.decode('utf-8') + "','superadmin', '" + str(site_group_id).strip() + "');\""
 astr = astr.replace('$', '\\$')
 print(astr)
 os.system(astr)
