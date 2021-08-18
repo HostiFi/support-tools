d7612c10b28825255246df1a1017aedec0a271f6
Reilly Chase <ReillyChase@MacBook-Pro.local>
2021-08-18 15:35:27 -0400

    UNMS 1.2 create admin bugfix

--- uisp/archived/create-super-admin.sh
+++ uisp/archived/create-super-admin.sh
@@ -2,4 +2,4 @@ echo "Username of UNMS Super Admin to create: "
 read USERNAME
 apt-get install python3-pip -y
 pip3 install bcrypt
-/usr/bin/python3 ../lib/uisp/py/create-super-admin.py -u $USERNAME
+/usr/bin/python3 ../lib/uisp/py/archived/create-super-admin.py -u $USERNAME
