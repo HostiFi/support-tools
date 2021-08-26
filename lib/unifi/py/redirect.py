bd7f61943048337b3cd2b9f473a690d854c66007
Reilly Chase <ReillyChase@MacBook-Pro.localdomain>
2021-08-26 12:59:10 -0500

    HTML redirect for UniFi finished

--- lib/unifi/py/redirect.py
+++ lib/unifi/py/redirect.py
@@ -1,2 +1,14 @@
-rm /var/www/html/index.html
-<meta http-equiv="refresh" content="0; URL='https:// site goes here :8443'" />
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d','--domain', help='Domain name to redirect 80, 443 to 8443 for')
+args = parser.parse_args()
+
+os.system("rm /var/www/html/index.html")
+os_cmd = """cat > /var/www/html/index.html << EOF
+<meta http-equiv="refresh" content="0; URL='https://""" + args.domain + """:8443'" />
+EOF
+"""
+os.system(os_cmd)
+print("Port 80, 443 are now redirected to https://" + args.domain + ":8443")
--- unifi/redirect.sh
+++ unifi/redirect.sh
@@ -1,1 +1,4 @@
-s
+#!/bin/bash
+echo "Domain name to redirect 80, 443 to 8443 for: "
+read DOMAIN
+/usr/bin/python3 ../lib/unifi/py/redirect.py -d $DOMAIN
