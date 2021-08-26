96adbc2dcc4c8d2f226a227afd8da7fe3885272a
Reilly Chase <ReillyChase@MacBook-Pro.localdomain>
2021-08-25 23:06:19 -0500

    WIP

--- unifi/maintenance-mode.sh
+++ unifi/maintenance-mode.sh
@@ -3,4 +3,4 @@ echo $HOSTNAME
 IFS='.' read -r SERVER string <<< "$HOSTNAME"
 a=".hostifi.com"
 SERVER+=$a
-/usr/bin/python3 ../lib/unifi/py/maintenance-mode.py -s $SERVER
+/usr/bin/python3 ../lib/unifi/zabbix/maintenance-mode.py -s $SERVER
