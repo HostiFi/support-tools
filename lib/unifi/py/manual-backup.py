ba9a7c314b75c37488067e1403cbbc34c5ea06f7
Reilly Chase <ReillyChase@MacBook-Pro.local>
2021-09-06 15:49:12 -0400

    Bugfix

--- lib/unifi/py/manual-backup.py
+++ lib/unifi/py/manual-backup.py
@@ -38,17 +38,21 @@ try:
     r = subprocess.check_output(["/usr/bin/python3", os.path.join(dir_path, 'create-super-admin.py'), "-u", user, "-e", "support@hostifi.com"])
 except subprocess.CalledProcessError as e:
     r = e.r
-password = re.findall("Password: (.+)\n", r.decode('ascii'))[0]
-unifi_server = UniFi(hostname, user, password)
-unifi_server.login()
-unifi_version = get_unifi_version(hostname)
-r = unifi_server.create_manual_backup()
-backup_dl_link = "https://" + hostname + ":8443" + r
-today = datetime.date.today()
-path_to_backup = "/usr/lib/unifi/data/backup/manual_" + unifi_version + "_" + today.strftime("%Y%m%d") + "_" + str(int(time.time())) + ".unf"
-unifi_server.download_backup(path_to_backup, backup_dl_link)
-unifi_server.logout()
-os.system("/usr/bin/python3 " + dir_path + "/delete-super-admin.py -u " + user)
-backup_size_in_bytes = os.stat(path_to_backup).st_size
-print("Manual backup stored at: " + path_to_backup)
-print("Backup size: " + human_readable(backup_size_in_bytes))
+try:
+    password = re.findall("Password: (.+)\n", r.decode('ascii'))[0]
+    unifi_server = UniFi(hostname, user, password)
+    unifi_server.login()
+    unifi_version = get_unifi_version(hostname)
+    r = unifi_server.create_manual_backup()
+    backup_dl_link = "https://" + hostname + ":8443" + r
+    today = datetime.date.today()
+    path_to_backup = "/usr/lib/unifi/data/backup/manual_" + unifi_version + "_" + today.strftime("%Y%m%d") + "_" + str(int(time.time())) + ".unf"
+    unifi_server.download_backup(path_to_backup, backup_dl_link)
+    unifi_server.logout()
+    os.system("/usr/bin/python3 " + dir_path + "/delete-super-admin.py -u " + user)
+    backup_size_in_bytes = os.stat(path_to_backup).st_size
+    print("Manual backup stored at: " + path_to_backup)
+    print("Backup size: " + human_readable(backup_size_in_bytes))
+except:
+    # Delete the admin account if anything fails
+    os.system("/usr/bin/python3 " + dir_path + "/delete-super-admin.py -u " + user)
