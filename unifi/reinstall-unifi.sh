80f34dde932519fbdb545bae67d881c633ac5918
Reilly Chase <ReillyChase@MacBook-Pro.local>
2021-09-14 17:46:35 -0400

    Restore WIP

--- unifi/reinstall-unifi.sh
+++ unifi/reinstall-unifi.sh
@@ -3,8 +3,8 @@ echo "Still in development, use the Notion guide for now! -rchase"
 #exit
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
-if find /usr/lib/unifi/data/backup/ -maxdepth 3 -name '*.unf' -type f -printf "%TY-%Tm-%Td %TT %p\n" | sort -r | head -1 | awk '{print $3}' > /dev/null; then
-	path_to_latest_backup=$(find /usr/lib/unifi/data/backup/ -maxdepth 3 -name '*.unf' -type f -printf "%TY-%Tm-%Td %TT %p\n" | sort -r | head -1 | awk '{print $3}')
+path_to_latest_backup=$(find /usr/lib/unifi/data/backup/ -maxdepth 3 -name '*.unf' -type f -printf "%TY-%Tm-%Td %TT %p\n" | sort -r | head -1 | awk '{print $3}')
+if test -f "$path_to_latest_backup"; then
 	echo "Backup to be restored:"
 	echo $path_to_latest_backup
 	echo "Backup date:"
