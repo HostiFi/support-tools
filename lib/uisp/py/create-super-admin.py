4f003f55a3659fabe5aa9eb687565a9cefe5d3b1
Reilly Chase <ReillyChase@MacBook-Pro.local>
2021-11-16 17:11:48 -0500

    Fix site group id bug

--- lib/uisp/py/create-super-admin.py
+++ lib/uisp/py/create-super-admin.py
@@ -9,6 +9,7 @@ import re
 parser = argparse.ArgumentParser()
 parser.add_argument('-u','--username', help='Username of UISP Super Admin to create')
 parser.add_argument('-p', '--password', help='UISP password to create')
+parser.add_argument('-s', '--site-group-id', help='Site group ID (optional)')
 parser.add_argument('-e', '--email', help='UISP email to create')
 
 args = parser.parse_args()
@@ -23,7 +24,10 @@ else:
 	email = args.email
 random_uuid = str(uuid.uuid4())
 bcrypt_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-site_group_id= os.popen('docker exec -it unms-postgres psql -U unms -d unms -t -c "SELECT group_id FROM access_group_site LIMIT 1;"').read()
+if args.site_group_id:
+	site_group_id = args.site_group_id
+else:
+	site_group_id = os.popen('docker exec -it unms-postgres psql -U unms -d unms -t -c "SELECT group_id FROM access_group_site LIMIT 1;"').read()
 astr = "docker exec -it unms-postgres psql -U unms -c \"INSERT INTO unms.user (id,username,email,password,role,site_group_id) VALUES ('" + random_uuid + "','" + args.username + "','" + email + "','" + bcrypt_hash.decode('utf-8') + "','superadmin', '" + str(site_group_id).strip() + "');\""
 astr = astr.replace('$', '\\$')
 print(astr)
