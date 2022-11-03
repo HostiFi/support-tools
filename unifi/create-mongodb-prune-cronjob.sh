8cad8ba572c44ca66c6159c75960839e3be2ad77
Andrew Soutar <andrew@andrewsoutar.com>
2022-11-03 13:21:44 -0400

    Convert prune from cronjob to timer

--- unifi/create-mongodb-prune-cronjob.sh
+++ unifi/create-mongodb-prune-cronjob.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
-crontab -l | { cat; echo "0 * * * * /usr/bin/mongo --port 27117 < /root/support-tools/lib/unifi/mongodb/prune.js"; } | crontab -
-echo "This server will now prune all UniFi statistics every minute"
+set -euo pipefail
+
+mkdir -p /etc/systemd/system
+cat > /etc/systemd/system/unifi-prune-mongodb.service <<'EOF'
+[Unit]
+Description=Prune statistics from the UniFi database
+Requires=unifi.service
+After=unifi.service
+
+[Service]
+Type=oneshot
+ExecStart=/bin/bash /root/support-tools/unifi/prune-mongodb.sh
+EOF
+
+cat > /etc/systemd/system/unifi-prune-mongodb.timer <<'EOF'
+[Unit]
+Description=Prune UniFi statistics every hour
+
+[Timer]
+OnCalendar=hourly
+
+[Install]
+WantedBy=timers.target
+EOF
+
+systemctl daemon-reload
+systemctl enable --now unifi-prune-mongodb.timer
+
+echo "This server will now prune all UniFi statistics every hour"
