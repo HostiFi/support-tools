f324372afc363fb5a620118a43121ee8e9422045
Reilly Chase <rchase@rchase.com>
2023-01-05 15:07:16 -0500

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,6 +3,7 @@ apt-get update -y
 apt-get install python3 -y
 apt-get install python3-pip -y
 apt-get install build-essential libssl-dev libffi-dev python-dev -y
+pip3 install pyasn1
 pip3 install pymongo
 pip3 install bcrypt
 pip3 install pyzabbix
