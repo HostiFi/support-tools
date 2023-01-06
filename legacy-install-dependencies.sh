4fdb44a85910d9b09aa01f1efd53fc77aa4dedc5
Reilly Chase <rchase@rchase.com>
2023-01-06 16:37:13 -0500

    Update legacy-install-dependencies.sh

--- legacy-install-dependencies.sh
+++ legacy-install-dependencies.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+apt-get update -y
+apt-get install python3 -y
+apt-get install python3-pip -y
+apt-get install build-essential libssl-dev libffi-dev python-dev -y
 apt-get install python3-pyasn1 -y
 apt-get install python3-setuptools -y
-bash /root/support-tools/install-dependencies.sh
-pip3 install pymongo==3.5.
+pip3 install pymongo==3.5.1
+pip3 install bcrypt
+pip3 install pyzabbix
+
