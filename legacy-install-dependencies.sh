3010e96637163bdc310a55afc2fc174f0200db8a
Reilly Chase <rchase@rchase.com>
2023-01-06 16:47:09 -0500

    Update legacy-install-dependencies.sh

--- legacy-install-dependencies.sh
+++ legacy-install-dependencies.sh
@@ -5,6 +5,7 @@ apt-get install python3-pip -y
 apt-get install build-essential libssl-dev libffi-dev python3-dev -y
 apt-get install python3-pyasn1 -y
 apt-get install python3-setuptools -y
+pip3 install cffi
 pip3 install pymongo==3.5.1
 pip3 install bcrypt==3.1.7
 pip3 install pyzabbix==0.7.4
