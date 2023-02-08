0d947e278b341b856e2ef4b53cea5c5ed20dd19f
Reilly Chase <rchase@rchase.com>
2023-02-08 14:26:22 -0500

    Add find and delete device bash scripts

---
+++ omada/delete-device.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "MAC of device to delete: "
+read MAC
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+/usr/bin/python3 ../lib/omada/py/delete-device.py -m $MAC
