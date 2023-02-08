62b79cff02da131a7315ed5cf8144dc82eda74b4
Andrew Soutar <andrew@andrewsoutar.com>
2023-02-08 14:55:56 -0500

    General cleanups

--- omada/delete-device.sh
+++ omada/delete-device.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-echo "MAC of device to delete: "
-read MAC
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path"
-/usr/bin/python3 ../lib/omada/py/delete-device.py -m $MAC
+read -p "MAC of device to delete: " MAC
+cd "$(dirname "${BASH_SOURCE[0]}")"
+/usr/bin/python3 ../lib/omada/py/delete-device.py -m "$MAC"
--- omada/find-device.sh
+++ omada/find-device.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-echo "MAC of device to find: "
-read MAC
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path"
-/usr/bin/python3 ../lib/omada/py/find-device.py -m $MAC
+read -p "MAC of device to find: " MAC
+cd "$(dirname "${BASH_SOURCE[0]}")"
+/usr/bin/python3 ../lib/omada/py/find-device.py -m "$MAC"
