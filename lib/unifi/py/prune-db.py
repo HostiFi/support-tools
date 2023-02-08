f1e50130911ed4eedacdb425c8ebb82bd2659271
Reilly Chase <rchase@rchase.com>
2023-02-08 12:01:36 -0500

    Memory improvements from Andrew

--- lib/unifi/py/prune-db.py
+++ lib/unifi/py/prune-db.py
@@ -5,41 +5,16 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-f','--file', help='db file name with path')
 args = parser.parse_args()
 
-encoded_docs = []
-current_collection = ""
+collection = ""
 
-input_db = open(args.file, 'rb')
-output_db = open(args.file + "_new", 'wb')
-
-for doc in bson.decode_file_iter(input_db):
-    if 'collection' in doc:
-        current_collection = doc['collection']
-        encoded_doc = bson.encode(doc)
-        encoded_docs.append(encoded_doc)
-        continue
-    if current_collection == "event":
-        continue
-    if current_collection == "alarm":
-        continue
-    if current_collection == "rogue":
-        continue
-    if current_collection == "voucher":
-        continue
-    if current_collection == "guest":
-        continue
-    if current_collection == "user":
-        if 'use_fixedip' in doc or 'blocked' in doc or 'noted' in doc:
-            encoded_doc = bson.encode(doc)
-            encoded_docs.append(encoded_doc)
+with open(args.file, "rb") as file_in, open(args.file + "_new", "wb") as file_out:
+    for doc in bson.decode_file_iter(file_in):
+        if "__cmd" in doc:
+            if doc["__cmd"] != "select":
+                raise RuntimeError(f"Unknown command: " + doc["__cmd"])
+            collection = doc["collection"]
+        elif collection in ("event", "alarm", "rogue", "voucher", "guest"):
             continue
-        else:
+        elif collection == "user" and not any(key in doc for key in ("use_fixedip", "blocked", "noted")):
             continue
-    encoded_doc = bson.encode(doc)
-    encoded_docs.append(encoded_doc)
-
-
-for doc in encoded_docs:
-    output_db.write(doc)
-
-input_db.close()
-output_db.close()
+        file_out.write(bson.encode(doc))
