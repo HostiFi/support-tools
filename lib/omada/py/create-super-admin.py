81a85050e8c52f410084619327ab4b0b4b366171
Reilly Chase <rchase@rchase.com>
2023-02-14 12:59:39 -0500

    Omada 5.8 db changes, bug fix

--- lib/omada/py/create-super-admin.py
+++ lib/omada/py/create-super-admin.py
@@ -57,7 +57,7 @@ def create_super_admin(password):
             "devices_upgrade_notification" : False,
         }).inserted_id
     else:
-        new_tenant_id = mdb.user.insert_one({
+        new_tenant_id = mdb.tenant.insert_one({
             "name" : args.username,
             "password" : sha256_crypt(password),
             "email" : base64.b64encode(args.email.encode('utf-8')).decode('ascii'),
