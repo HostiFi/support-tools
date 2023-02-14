6a482a515fcfa58a58d28bb2fe111acd967bde47
Reilly Chase <rchase@rchase.com>
2023-02-14 12:36:56 -0500

    Omada 5.8 db changes

--- lib/omada/py/create-super-admin.py
+++ lib/omada/py/create-super-admin.py
@@ -34,35 +34,44 @@ def create_super_admin(password):
     mdb = client.omada
     site_ids = []
     logging.info("Gathering omadac_id...")
-    omadac_id = None
-    # Check in a few places to try to find omadac_id
-    for configversion in mdb.configversion.find():
-        omadac_id = configversion["omadac_id"]
-    if omadac_id == None:
-        for user in mdb.user.find():
-            omadac_id = user["omadac_id"]
-    if omadac_id == None:
-        for maintenanceomadacsetting in mdb.maintenanceomadacsetting.find():
-            omadac_id = maintenanceomadacsetting["omadac_id"]
+    omadac_id = mdb.omadac.find_one()["_id"]
+    omada_db_version = mdb.systemsetting.find_one()["start_up_info"]["db_version"]
     logging.info("Gathering side ids...")
     for site in mdb.site.find():
         site_ids.append(str(site["_id"]))
     logging.info("Inserting User...")
-    new_admin_id = mdb.user.insert_one({
-        "name" : args.username,
-        "password" : sha256_crypt(password),
-        "email" : base64.b64encode(args.email.encode('utf-8')).decode('ascii'),
-        "omadac_id" : omadac_id,
-        "role_type" : 0,
-        "verified" : True,
-        "permissions" : [ "license", "site", "read", "adopt", "admin", "write", "manage" ],
-        "site_ids" : site_ids,
-        "alert" : True,
-        "all_site" : True,
-        "last_site" : "Default",
-        "time_created" : int(datetime.utcnow().timestamp()),
-        "devices_upgrade_notification" : False,
-    }).inserted_id
+    if omada_db_version < "5.8.0":
+        new_admin_id = mdb.user.insert_one({
+            "name" : args.username,
+            "password" : sha256_crypt(password),
+            "email" : base64.b64encode(args.email.encode('utf-8')).decode('ascii'),
+            "omadac_id" : omadac_id,
+            "role_type" : 0,
+            "verified" : True,
+            "permissions" : [ "license", "site", "read", "adopt", "admin", "write", "manage" ],
+            "site_ids" : site_ids,
+            "alert" : True,
+            "all_site" : True,
+            "last_site" : site_ids[0],
+            "time_created" : int(datetime.utcnow().timestamp()),
+            "devices_upgrade_notification" : False,
+        }).inserted_id
+    else:
+        new_admin_id = mdb.user.insert_one({
+            "name" : args.username,
+            "password" : sha256_crypt(password),
+            "email" : base64.b64encode(args.email.encode('utf-8')).decode('ascii'),
+            "omadac_id" : omadac_id,
+            "user_type": 0,
+            "role_id" : "master_admin_id",
+            "verified" : True,
+            "site_ids" : site_ids,
+            "alert" : True,
+            "all_site" : True,
+            "last_site" : site_ids[0],
+            "time_created" : int(datetime.utcnow().timestamp()),
+            "devices_upgrade_notification" : False,
+        }).inserted_id
 
     print("Omada Super Admin created")
     print("Username: " + args.username)
