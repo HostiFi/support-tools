b93b4b5ce3178c54d9989f43aef211537eb8782b
Andrew Soutar <andrew@andrewsoutar.com>
2023-09-04 16:08:25 -0400

    Rewrite prune-backup in Java

---
+++ lib/unifi/java/PruneBackup.java
@@ -0,0 +1,127 @@
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.bson.BSONObject;
+import org.bson.BasicBSONDecoder;
+import org.bson.BasicBSONEncoder;
+
+public class PruneBackup {
+    public static SecretKeySpec KEY =
+        new SecretKeySpec("bcyangkmluohmars".getBytes(), "AES");
+    public static IvParameterSpec IV =
+        new IvParameterSpec("ubntenterpriseap".getBytes());
+
+    private static Cipher makeCipher(int mode) {
+        try {
+            var cipher = Cipher.getInstance("AES/CBC/NoPadding");
+            cipher.init(mode, KEY, IV);
+            return cipher;
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private static ZipInputStream openBackupInput(InputStream in) {
+        var cis = new CipherInputStream(in, makeCipher(Cipher.DECRYPT_MODE));
+        return new ZipInputStream(cis);
+    }
+
+    private static ZipOutputStream openBackupOutput(OutputStream out) {
+        var cos = new CipherOutputStream(out, makeCipher(Cipher.ENCRYPT_MODE));
+        return new ZipOutputStream(cos);
+    }
+
+    private static void pruneDbGz(InputStream in, OutputStream out)
+        throws IOException {
+
+        var decoder = new BasicBSONDecoder();
+        var encoder = new BasicBSONEncoder();
+        var bsonIn = new GZIPInputStream(in);
+        var bsonOut = new GZIPOutputStream(out);
+        try {
+            for (String collection = null;;) {
+                BSONObject bson;
+                try {
+                    bson = decoder.readObject(bsonIn);
+                } catch (EOFException ex) {
+                    break;
+                }
+                if (bson.containsField("__cmd")) {
+                    if (!bson.get("__cmd").equals("select")) {
+                        throw new RuntimeException("Invalid command in bson");
+                    }
+                    collection = (String) bson.get("collection");
+                } else {
+                    switch (collection) {
+                    case "alert":
+                    case "rogue":
+                    case "alarm":
+                    case "event":
+                    case "voucher":
+                    case "guest":
+                        continue;
+
+                    case "user":
+                        if (!(bson.containsField("use_fixedip") ||
+                              bson.containsField("noted") ||
+                              bson.containsField("blocked"))) {
+                            continue;
+                        }
+                    }
+                }
+                bsonOut.write(encoder.encode(bson));
+            }
+        } finally {
+            bsonOut.finish();
+        }
+    }
+
+    public static void main(String... args) throws IOException {
+        if (args.length != 2) {
+            System.err.println("Usage: PruneBackup in_file out_file");
+            System.exit(2);
+        }
+
+        var in_file = args[0];
+        var out_file = args[1];
+
+        try (var in = openBackupInput(new FileInputStream(in_file));
+             var out = openBackupOutput(new FileOutputStream(out_file))) {
+            for (;;) {
+                var entry = in.getNextEntry();
+                if (entry == null)
+                    break;
+
+                out.putNextEntry(entry);
+
+                switch (entry.getName()) {
+                case "db_stat.gz":
+                    new GZIPOutputStream(out).finish();
+                    break;
+
+                case "db.gz":
+                    pruneDbGz(in, out);
+                    break;
+
+                default:
+                    in.transferTo(out);
+                }
+            }
+        }
+    }
+}
--- unifi/prune-backup.sh
+++ unifi/prune-backup.sh
@@ -17,53 +17,5 @@ fi
 
 declare -a mongo_jar=(/usr/lib/unifi/lib/mongo-java-driver-*.jar)
 
-java jdk.internal.jshell.tool.JShellToolProvider "-R-Din_file=${in_file}" "-R-Dout_file=${out_file}" --class-path="${mongo_jar[0]}" - <<'EOF'
-var key = new javax.crypto.spec.SecretKeySpec("bcyangkmluohmars".getBytes(), "AES");
-var iv = new javax.crypto.spec.IvParameterSpec("ubntenterpriseap".getBytes());
-
-var decryptCipher = javax.crypto.Cipher.getInstance("AES/CBC/NoPadding");
-decryptCipher.init(javax.crypto.Cipher.DECRYPT_MODE, key, iv);
-var unzip = new java.util.zip.ZipInputStream(new javax.crypto.CipherInputStream(new java.io.FileInputStream(System.getProperty("in_file")), decryptCipher));
-
-var encryptCipher = javax.crypto.Cipher.getInstance("AES/CBC/NoPadding");
-encryptCipher.init(javax.crypto.Cipher.ENCRYPT_MODE, key, iv);
-var rezip = new java.util.zip.ZipOutputStream(new javax.crypto.CipherOutputStream(new java.io.FileOutputStream(System.getProperty("out_file")), encryptCipher));
-
-for (;;) {
-    var entry = unzip.getNextEntry();
-    if (entry == null)
-        break;
-    rezip.putNextEntry(entry);
-    if (entry.getName().equals("db_stat.gz")) {
-        new java.util.zip.GZIPOutputStream(rezip).finish();
-    } else if (entry.getName().equals("db.gz")) {
-        var bsonStream = new java.util.zip.GZIPInputStream(unzip);
-        var bsonOutStream = new java.util.zip.GZIPOutputStream(rezip);
-        var bsonDecoder = new org.bson.BasicBSONDecoder();
-        var bsonEncoder = new org.bson.BasicBSONEncoder();
-        String collection = null;
-        while (bsonStream.available() != 0) {
-            var bson = bsonDecoder.readObject(bsonStream);
-            if (bson.containsField("__cmd")) {
-                if (!bson.get("__cmd").equals("select")) {
-                    throw new RuntimeException("Invalid command in bson");
-                }
-                collection = (String) bson.get("collection");
-            } else if (collection.equals("alert") ||
-                       collection.equals("rogue") ||
-                       collection.equals("alarm") ||
-                       collection.equals("event") ||
-                       collection.equals("voucher") ||
-                       collection.equals("guest") ||
-                       (collection.equals("user") && !(bson.containsField("use_fixedip") || bson.containsField("noted") || bson.containsField("blocked")))) {
-                continue;
-            }
-            bsonOutStream.write(bsonEncoder.encode(bson));
-        }
-    } else {
-        unzip.transferTo(rezip);
-    }
-}
-
-rezip.close();
-EOF
+java -classpath "${BASH_SOURCE[0]%/*}/../lib/unifi/java:${mongo_jar[0]}" \
+     PruneBackup "$in_file" "$out_file"
