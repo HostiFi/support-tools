9bd46345c3d6bb8506b75e6217a480c288bbc2f8
Andrew Soutar <andrew@andrewsoutar.com>
2023-10-18 16:26:00 -0400

    [omada] Improve compatibility with older Python versions

--- lib/omada/py/omada.py
+++ lib/omada/py/omada.py
@@ -2,11 +2,11 @@ import functools
 
 import pymongo
 
-@functools.cache
+@functools.lru_cache(max_size=None)
 def db():
     return pymongo.MongoClient("mongodb://127.0.0.1:27217").omada
 
-@functools.cache
+@functools.lru_cache(max_size=None)
 def version():
     version_str = db().systemsetting.find_one()["start_up_info"]["db_version"]
     return tuple(map(int, version_str.split(".")))
