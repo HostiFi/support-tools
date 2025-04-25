17d39a3405f0dc2d0e7957c825bce558d2d29a4c
Reilly Chase <rchase@rchase.com>
2025-04-25 16:32:36 -0400

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -4,7 +4,6 @@ apt-get install python3 -y
 apt-get install python3-pip -y
 apt-get install python3-pymongo -y
 apt-get install build-essential libssl-dev libffi-dev python3-dev -y
-pip3 install pymongo
 pip3 install bcrypt
 pip3 install pyzabbix
 pip3 uninstall pymongo bson
