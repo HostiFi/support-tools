f81365f979c12f0253e7b4333795f0351e0638f7
Reilly Chase <rchase@Mac.localdomain>
2025-04-30 16:57:34 -0400

    Ownership tools update

--- lib/unifi/py/check-owner.py
+++ lib/unifi/py/check-owner.py
@@ -5,15 +5,17 @@ from bson import ObjectId
 
 client = pymongo.MongoClient("mongodb://127.0.0.1:27117/ace")
 mdb = client.ace
+try:
+    super_cloudaccess = mdb.setting.find_one({"key": "super_cloudaccess"})
+    ubic_uuid = super_cloudaccess["ubic_uuid"]
+    admin = mdb.admin.find_one({"ubic_uuid": ubic_uuid})
+    owner_email = admin["email"]
+    owner_name = admin["name"]
+    owner_ubic_name = admin["ubic_name"]
 
-super_cloudaccess = mdb.setting.find_one({"key": "super_cloudaccess"})
-ubic_uuid = super_cloudaccess["ubic_uuid"]
-admin = mdb.admin.find_one({"ubic_uuid": ubic_uuid})
-owner_email = admin["email"]
-owner_name = admin["name"]
-owner_ubic_name = admin["ubic_name"]
-
-print("Owner")
-print("Email: " + owner_email)
-print("Username: " + owner_name)
-print("UI SSO: " + owner_ubic_name)
+    print("Owner")
+    print("Email: " + owner_email)
+    print("Username: " + owner_name)
+    print("UI SSO: " + owner_ubic_name)
+except:
+    print("No owner found")
