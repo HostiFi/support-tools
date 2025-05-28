1082dfdac4d139f8eeb3870f7704459cf2961aac
Reilly Chase <rchase@rchase.com>
2025-05-28 12:22:27 -0400

    Update create-super-admin.py

--- lib/omada/py/create-super-admin.py
+++ lib/omada/py/create-super-admin.py
@@ -18,7 +18,7 @@ parser.add_argument('-p', '--password', help='Omada password to create')
 args = parser.parse_args()
 
 randchoice = SystemRandom().choice
-password = ''.join(random.choice(string.ascii_letters) for i in range(8))
+password = 'H137!'.join(random.choice(string.ascii_letters) for i in range(8))
 
 def sha256_crypt(password):
     iterations = 500000
